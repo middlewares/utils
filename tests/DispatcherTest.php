f6248494af1fe7484b6acdee7594579989af8e98
oscarotero <oom@oscarotero.com>
2017-05-06 11:08:37 +0200

    removed duplicated test

--- tests/DispatcherTest.php
+++ tests/DispatcherTest.php
@@ -4,39 +4,11 @@ namespace Middlewares\Tests;
 
 use Middlewares\Utils\Dispatcher;
 use Middlewares\Utils\CallableMiddleware;
-use Zend\Diactoros\ServerRequest;
+use Psr\Http\Message\ResponseInterface;
 
 class DispatcherTest extends \PHPUnit_Framework_TestCase
 {
     public function testDispatcher()
-    {
-        $dispatcher = new Dispatcher([
-            function ($request, $delegate) {
-                $response = $delegate->process($request);
-                $response->getBody()->write('3');
-
-                return $response;
-            },
-            function ($request, $delegate) {
-                $response = $delegate->process($request);
-                $response->getBody()->write('2');
-
-                return $response;
-            },
-            new CallableMiddleware(function ($request, $delegate) {
-                echo '1';
-
-                return $delegate->process($request);
-            }),
-        ]);
-
-        $response = $dispatcher->dispatch(new ServerRequest());
-
-        $this->assertInstanceOf('Psr\\Http\\Message\\ResponseInterface', $response);
-        $this->assertEquals('123', (string) $response->getBody());
-    }
-
-    public function testDispatcherHelper()
     {
         $response = Dispatcher::run([
             function ($request, $delegate) {
@@ -58,7 +30,7 @@ class DispatcherTest extends \PHPUnit_Framework_TestCase
             }),
         ]);
 
-        $this->assertInstanceOf('Psr\\Http\\Message\\ResponseInterface', $response);
+        $this->assertInstanceOf(ResponseInterface::class, $response);
         $this->assertEquals('123', (string) $response->getBody());
     }
 
@@ -66,8 +38,6 @@ class DispatcherTest extends \PHPUnit_Framework_TestCase
     {
         $this->expectException('UnexpectedValueException');
 
-        $response = Dispatcher::run([
-            ''
-        ]);
+        $response = Dispatcher::run(['']);
     }
 }
