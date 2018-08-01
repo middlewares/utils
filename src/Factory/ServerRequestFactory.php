f333d3c0c1f136b0acbae760041d36e4f8f18343
Marco Spengler <marco.spengler@outlook.de>
2018-08-01 12:51:56 +0200

    Exchanged abandoned http-interop/http-factory with psr/http-factory

--- src/Factory/ServerRequestFactory.php
+++ src/Factory/ServerRequestFactory.php
@@ -3,7 +3,7 @@ declare(strict_types = 1);
 
 namespace Middlewares\Utils\Factory;
 
-use Interop\Http\Factory\ServerRequestFactoryInterface;
+use Psr\Http\Message\ServerRequestFactoryInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use RuntimeException;
 
